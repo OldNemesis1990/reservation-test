a2f5ba76687f1d81e6f06c119e0ebdf3fc9d016b
Lee Baartman lee.baartman@gmail.com <lee.baartman@gmail.com>
2024-02-05 22:05:09 +0200

    Adjustments made to class name and file name

--- app/Http/Controllers/Guest/Dashboard.php
+++ app/Http/Controllers/Guest/Dashboard.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\guest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class dashboard extends Controller
+class Dashboard extends Controller
 {
     //
 }
