a63b221f985046f5150ed01d932be163c8eb69c6
Lee Baartman lee.baartman@gmail.com <lee.baartman@gmail.com>
2024-02-11 23:00:25 +0200

    Configured the routes with security

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,9 @@ use Inertia\Inertia;
 //Call Controllers
 use App\Http\Controllers\Guest\Dashboard as GuestDashboardController;
 use App\Http\Controllers\Staff\Dashboard as StaffDashboardController;
+use App\Http\Controllers\AccommodationBookingController as ReservationController;
+use App\Http\Controllers\AccommodationController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +23,7 @@ use App\Http\Controllers\Staff\Dashboard as StaffDashboardController;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
+    return Inertia::render('HomePage', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
@@ -28,59 +31,85 @@ Route::get('/', function () {
     ]);
 });
 
+/** ======== ACCOMMODATION ROUTES FOR PUBLIC ======== */
+Route::resource('/accommodations', AccommodationController::class)
+->only(['index', 'show']); // Specify only the needed routes
+Route::post('/accommodation-search', [AccommodationController::class, 'searchAccommodations'])->name('accommodations.search');
+
 // Routes for guests on login or registration
 Route::middleware(['auth'])->group( function() {
-    Route::get('/dashboard/profile', [GuestDashboardController::class, 'index'])->name('dashboard.profile');
+    Route::resource('/user-dashboard', GuestDashboardController::class)
+    ->only(['index'])
+    ->names([
+        'index' => 'guest.dashboard', // Custom route name for the index method
+    ]);
 });
 
 // Routes for staff members
 Route::middleware(['auth', 'redirectIfNotStaff'])->group( function() {
-    /** ======== RESERVATIONS ROUTES ======== */
-    Route::get('/dashboard/reservations', [StaffDashboardController::class, 'getReservationsList'])->name('dashboard.reservations.list'); // Routes for reservations list
-    Route::post('/dashboard/create-reservation', [StaffDashboardController::class, 'createReservation'])->name('dashboard.reservation.create'); // Route to Create reservation
-    Route::put('/dashboard/update-reservation/{id}', [StaffDashboardController::class, 'updateReservation'])->name('dashboard.reservation.update'); // Route to view and update reservation
-    Route::delete('/dashboard/delete-reservation/{id}', [StaffDashboardController::class, 'deleteReservation'])->name('dashboard.reservation.delete'); // Route to delete reservation
-    Route::post('/dashboard/search-reservation', [StaffDashboardController::class, 'searchReservation'])->name('dashboard.reservation.search'); // Route to search reservation
-    Route::post('/dashboard/search-client', [StaffDashboardController::class, 'searchclient'])->name('dashboard.client.search'); // Route for client search
-
     /** ======== ACCOMMODATION ROUTES FOR THIS GROUP AND HIGHER ======== */
-    Route::get('/dashboard/acommodations', [StaffDashboardController::class, 'getAccommodationsList'])->name('dashboard.accommodations.list'); // Routes for accommodations list
-    Route::get('/dashboard/acommodations/{id}', [StaffDashboardController::class, 'getAccommodation'])->name('dashboard.accommodations.view'); // Routes for accommodations view, this group cannot update acommodations
-    Route::post('/dashboard/acommodation-search', [StaffDashboardController::class, 'searchAccommodations'])->name('dashboard.accommodations.search'); // Routes for accommodations search
+    Route::resource('/dashboard/accommodations', AccommodationController::class)
+    ->only(['index', 'show']); // Specify only the needed routes
+    Route::post('/dashboard/accommodation-search', [AccommodationController::class, 'searchAccommodations'])->name('dashboard.accommodations.search');
 
+    Route::resource('/staff-dashboard', StaffDashboardController::class)
+    ->only(['index'])
+    ->names([
+        'index' => 'staff.dashboard', // Custom route name for the index method
+    ]);
+    /** ======== RESERVATIONS ROUTES ======== */
+    Route::resource('/dashboard/reservations', ReservationController::class)
+    ->only(['index', 'create', 'show', 'store', 'edit', 'update', 'destroy']); // Specify only the needed routes
+    Route::post('/dashboard/search-reservation', [ReservationController::class, 'searchReservation'])->name('dashboard.reservation.search');
+    Route::post('/dashboard/search-idnumber', [ReservationController::class, 'idNumber'])->name('dashboard.reservation.idnumber');
+    Route::post('/dashboard/reservation/check-in', [ReservationController::class, 'checkIn'])->name('dashboard.reservation.checkIn');
 });
 
 // Routes for senior staff members
 Route::middleware(['auth', 'redirectIfNotSeniorStaff'])->group( function() {
     /** ======== STAFF ROUTES ======== */    
-    Route::get('/dashboard/staff', [StaffDashboardController::class, 'getStaffList'])->name('dashboard.staff.list'); // Routes to view staff list    
-    Route::post('/dashboard/create-staff', [StaffDashboardController::class, 'createStaffMember'])->name('dashboard.staff.create'); // Routes to create staff member    
-    Route::put('/dashboard/update-staff/{id}', [StaffDashboardController::class, 'updateStaffMember'])->name('dashboard.staff.update'); // Routes to view and update staff member
-    Route::delete('/dashboard/delete-staff/{id}', [StaffDashboardController::class, 'deleteStaffMember'])->name('dashboard.staff.delete'); // Routes to delete staff member
-    Route::post('/dashboard/search-staff', [StaffDashboardController::class, 'searchStaffMember'])->name('dashboard.staff.search'); // Routes to search for staff member 
+    // Resourceful routes for staff members
+    Route::resource('/dashboard/staff', UserController::class)
+    ->only(['index', 'show', 'store', 'update', 'destroy']); // Specify only the needed routes
+
+    // Custom route for searching staff members
+    Route::post('/dashboard/staff/search', [UserController::class, 'searchStaffMember'])
+    ->name('dashboard.staff.search');
 
     /** ======== ACCOMMODATION ROUTES FOR THIS GROUP AND HIGHER ======== */ 
-    Route::post('/dashboard/acommodation-create', [StaffDashboardController::class, 'createAccommodations'])->name('dashboard.accommodations.create'); // Routes for accommodations list
-    Route::put('/dashboard/acommodation-update', [StaffDashboardController::class, 'updateAccommodations'])->name('dashboard.accommodations.update'); // Routes for accommodations view and update, for this group and higher
-    Route::delete('/dashboard/acommodation-delete', [StaffDashboardController::class, 'deleteAccommodations'])->name('dashboard.accommodations.delete'); // Routes for accommodations delete
+    // Resourceful routes for accommodations
+    Route::resource('/dashboard/accommodation', AccommodationController::class)
+    ->only(['create', 'store', 'edit', 'update', 'destroy']); // Specify only the needed routes
+    
 
-    /** ======== RESORT CONFIGURATIONS ROUTES FOR THIS GROUP AND HIGHER ======== */
-    Route::get('/dashboard/resort-amenities', [StaffDashboardController::class, 'resortAmenityList'])->name('dashboard.resort-amenities.list'); // Route to get a list of the resort amenities
-    Route::post('/dashboard/create-resort-amenities', [StaffDashboardController::class, 'createResortAmenity'])->name('dashboard.resort-amenities.create'); // Route to create the resort amenities
-    Route::put('/dashboard/update-resort-amenities', [StaffDashboardController::class, 'updateResortAmenity'])->name('dashboard.resort-amenities.update'); // Route to update the resort amenities
-    Route::delete('/dashboard/delete-resort-amenities', [StaffDashboardController::class, 'deleteResortAmenity'])->name('dashboard.resort-amenities.delete'); // Route to delete the resort amenities
+    /** ======== RESORT CONFIGURATIONS ROUTES FOR THIS GROUP AND HIGHER (I will create this if I have enough Time) ======== */
+    Route::resource('/dashboard/configuration', StaffDashboardController::class)
+    ->only(['index', 'store', 'show', 'update', 'destroy']) // Specify only the needed routes
+    ->names([
+        'index' => 'staff.configurations', // Custom route name for the index method
+    ]);
 
-    Route::get('/dashboard/accommodation-amenity-list', [StaffDashboardController::class, 'accommodationAmenitiesList'])->name('dashboard.accommodation-amenity.list'); // Route to get a list of amenities which needs to be called in the accommodations create/update page for example sleeper, self catering, towels
-    Route::post('/dashboard/create-accommodation-amenity', [StaffDashboardController::class, 'createAccommodationAmenities'])->name('dashboard.accommodation-amenity.create'); // Route to create an amenity 
-    Route::put('/dashboard/update-accommodation-amenity', [StaffDashboardController::class, 'updateAccommodationAmenities'])->name('dashboard.accommodation-amenity.update'); // Route to update an amenity 
-    Route::delete('/dashboard/delete-accommodation-amenity', [StaffDashboardController::class, 'deleteAccommodationAmenities'])->name('dashboard.accommodation-amenity.delete'); // Route to delete an amenity
+    // Resourceful routes for resort amenities
+    Route::resource('/dashboard/resort-amenities', StaffDashboardController::class)
+    ->only(['index', 'create', 'store', 'show', 'update', 'destroy']); // Specify only the needed routes
+
+    // Custom route for searching resort amenities
+    Route::post('/dashboard/resort-amenities/search', [StaffDashboardController::class, 'searchResortAmenities'])
+    ->name('dashboard.resort-amenities.search');
+
+    Route::resource('/dashboard/accommodation-amenities', StaffDashboardController::class)
+    ->only(['index', 'store', 'show', 'update', 'destroy']); // Specify only the needed routes
+
+    // Custom route for searching accommodation amenities
+    Route::post('/dashboard/accommodation-amenities/search', [StaffDashboardController::class, 'searchAccommodationAmenities'])
+        ->name('dashboard.accommodation-amenities.search');
 });
 
 // Routes for admin members
 Route::middleware(['auth', 'redirectIfNotAdmin'])->group( function() {
-    Route::get('/dashboard/system-health', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard.systemhealth');
+    // Route::get('/dashboard/system-health', function () {
+    //     return Inertia::render('Dashboard');
+    // })->name('dashboard.systemhealth');
 });
 
 Route::get('/dashboard', function () {
