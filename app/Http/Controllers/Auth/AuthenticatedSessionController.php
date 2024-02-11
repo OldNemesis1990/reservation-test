685d1edb3f2e4e350495a7a14875bfcc6d981f60
Lee Baartman lee.baartman@gmail.com <lee.baartman@gmail.com>
2024-02-11 22:57:07 +0200

    Logic for roles and permissions

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -40,13 +40,17 @@ class AuthenticatedSessionController extends Controller
             $role = $user->roles->pluck('name')->first();
 
             // Redirect based on user role
-            if($role == 'guest' || $role == 'vip') {
-                return redirect(RouteServiceProvider::PROFILE);
-            }
-            elseif($role == 'admin' || $role == 'manager' || $role == 'clerk') {
-                return redirect(RouteServiceProvider::RESERVATIONS);
-            } else {
-                return redirect(RouteServiceProvider::PROFILE);
+            switch ($role) {
+                case 'guest':
+                case 'vip':
+                    return redirect(RouteServiceProvider::GUESTDASHBOARD);
+                case 'admin':
+                case 'manager':
+                case 'clerk':
+                    return redirect(RouteServiceProvider::STAFFDASHBOARD);
+                default:
+                    // Default redirection if the user has no role or unrecognized role
+                    abort(401);
             }
         }
     }
